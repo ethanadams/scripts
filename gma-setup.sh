59e85f2bcc114458b09017235ecc96a24970b19d
Ethan <ethan@storj.io>
2020-03-09 14:27:49 -0400

    fix clone

--- gma-setup.sh
+++ gma-setup.sh
@@ -5,5 +5,5 @@ wget https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.13.8.linux-amd64.tar.gz
 echo -e "\nexport PATH=$PATH:/usr/local/go/bin:~/go/bin\n" >> ~/.profile
 source ~/.profile
-git clone git@github.com:ethanadams/scripts.git
+git clone https://github.com/ethanadams/scripts.git
 cd ~/scripts/downloader && go install ./... && cd ~
