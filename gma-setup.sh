89162725d1f03abc32acbdf09137ab68a94334ea
Ethan <ethan@storj.io>
2020-03-09 14:39:47 -0400

    remove StrictHostKeyChecking for github

--- gma-setup.sh
+++ gma-setup.sh
@@ -5,5 +5,6 @@ wget https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.13.8.linux-amd64.tar.gz
 echo -e "\nexport PATH=$PATH:/usr/local/go/bin:~/go/bin\n" >> ~/.profile
 source ~/.profile
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git clone https://github.com/ethanadams/scripts.git
 cd ~/scripts/downloader && go install ./... && cd ~
